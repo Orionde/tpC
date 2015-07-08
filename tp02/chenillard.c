08411a30ec3ceef3ed51236b4eabf922147758a3
fuhonjud <Jorane.Congio-Hollard@etu.iut2.upmf-grenoble.fr>
2015-07-08 11:01:47 +0200

    Manque gestion erreur dans chenillard

--- tp02/chenillard.c
+++ tp02/chenillard.c
@@ -1,8 +1,83 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 #include <time.h>
+#include <stdio.h>
+
+void check(char *a, char *e, int *t, int *n, int nb, char **str);
+void temporisation(int n);
+void putstr(char *str);
+void chenillard(int nb, char **str)
+{
+
+	char a = '*';
+	char e = '.';
+	int t = 500;
+	int n = 10;
+	int i;
+	if(nb %2 == 1)
+	{
+		if(nb > 1)
+		{
+			check(&a, &e, &t, &n, nb, str);
+		}
+			putchar(str[1][0]);
+
+		char amp[n];
+
+		for(i = 0; i < n; i++)
+			amp[i] = e;
+
+		amp[n] = 0;//Ajout du caractère de fin de chaine
+		i = 0;
+
+		while(amp[i])
+		{
+			amp[i] = a;
+			if(i > 0)
+				amp[i - 1] = e;
+			write(1, "\r", 1);
+			putstr(amp);
+			temporisation(t);
+			i++;
+			if(amp[i] == 0)
+			{
+				amp[i - 1] = e;
+				i = 0;
+			}
+		}
+		write(1, "\n", 1);
+	}
+	else
+	{
+		putstr("Usage : chenillard_v2 [-a <car_ampoule_allumee>] [-e <car_ampoule_eteinte>] [-t <temporisation>] [-n <nombre_d_ampoules>]");
+		write(1, "\n", 1);
+	}
+}
+
+void check(char *a, char *e, int *t, int *n, int nb, char **str)
+{
+	int i = 1;
+	while(i < nb)
+	{
+		if(str[i][0] == '-')
+		{
+			if(str[i][1] == 'a')
+				*a = str[i+1][0];
+			if(str[i][1] == 'e')
+				*e = str[i+1][0];
+			if(str[i][1] == 't')
+				*t = atoi(str[i+1]);
+			if(str[i][1] == 'n')
+				*n = atoi(str[i+1]);
+			if(str[i][1] != 'a' && str[i][1] != 'e' && str[i][1] != 't' && str[i][1] != 'n')
+				printf("Argument inconnu : %c", str[i][1]);
+		}
+		else
+			putstr("erreur");
+		i += 2;
+	}
+}
 
 void temporisation(int n)
 {
@@ -21,64 +96,7 @@ void putstr(char *str)
 
 int main(int ac, char **av)
 {
-	char a = '*';
-	char e = '.';
-	int t = 500;
-	int n = 10;
-	int i = 0;
-	if(ac %2 == 1)
-	{
-		if(ac > 1)
-		{
-			
-			while(i < ac)
-			{
-				if(av[i][0] == '-')
-				{
-					if(av[i][0] == 'a')
-						a = av[i+1][0];
-					if(av[i][0] == 'e')
-						e = av[i+1][0];
-					if(av[i][0] == 't')
-						t = atoi(av[i+1]);
-					if(av[i][0] == 'n')
-						n = atoi(av[i+1]);
-				}
-				else
-					putstr("erreur");
-				i += 2;
-			}
-		}
-
-		char amp[n];
-
-		for(i = 0; i < n; i++)
-			amp[i] = e;
-
-		i = 0;
-
-		while(amp[i])
-		{
-			amp[i] = a;
-			if(i > 0)
-				amp[i - 1] = e;
-			i++;
-			write(1, "\r", 1);
-			putstr(amp);
-			temporisation(t);
-			if(amp[i] == 0)
-			{
-				amp[i - 1] = e;
-				i = 0;
-			}
-		}
-		write(1, "\n", 1);
-		return(0);
-	}
-	else
-	{
-		putstr("Usage : chenillard_v2 [-a <car_ampoule_allumee>] [-e <car_ampoule_eteinte>] [-t <temporisation>] [-n <nombre_d_ampoules>]");
-		write(1, "\n", 1);
-	}
-	return(1);
+	//chenillard(ac, av);
+	putchar(av[1][0]);
+	return(0);
 }
